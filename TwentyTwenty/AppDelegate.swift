71e39cac09f58c4d51f2cf710a23440125ab4d0d
Farzana Sultana <farzana25sm@gmail.com>
2016-07-28 11:51:10 +0600

    Notification switch status preserved

--- TwentyTwenty/AppDelegate.swift
+++ TwentyTwenty/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
         return true
     }
 
--- TwentyTwenty/ViewController.swift
+++ TwentyTwenty/ViewController.swift
@@ -9,28 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var elapsedTimeLabel: UILabel!
+    @IBOutlet var notificationSwicth: UISwitch!
+    
     let stopwatch = Stopwatch()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        guard let status = NSUserDefaults.standardUserDefaults().objectForKey("switchStatus") as? Bool else {
+            notificationSwicth.on = false
+            return
+        }
         
-        let notificationSettings =  UIUserNotificationSettings(
-            forTypes: [.Alert, .Badge, .Sound],
-            categories: nil)
-        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        notificationSwicth.on = status
+        setNotification(notificationSwicth)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func setNotification(sender: UISwitch) {
+        print(sender.on)
+        NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey: "switchStatus")
+        NSUserDefaults.standardUserDefaults().synchronize()
         
         if (sender.on) {
             print("Alarm On!")
@@ -38,6 +44,64 @@ class ViewController: UIViewController {
         } else {
             print("Alarm Off!")
             UIApplication.sharedApplication().cancelAllLocalNotifications()
+        }
+    }
+    
+    
+    func timerAction() {
+        
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        let settings = UIApplication.sharedApplication().currentUserNotificationSettings()
+        
+        if settings!.types == .None {
+            notificationSwicth.on = false
+            let ac = UIAlertController(title: "Can't schedule", message: "Either we don't have permission to schedule notifications in Settings, or we haven't asked yet.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+            return
+        }
+        
+        var notification = UILocalNotification()
+        notification = setUpNotification(notification)
+        notification.fireDate = NSDate.init(timeIntervalSinceNow: Double(5))
+        notification = publishLocalNotification(notification)
+        
+        for i in 1..<4 {
+            var notificationR = UILocalNotification()
+            notificationR = setUpNotification(notificationR)
+            notificationR.fireDate = NSDate.init(timeIntervalSinceNow: Double(i * 20 * 60))
+            notificationR = publishLocalNotification(notificationR)
+        }
+    }
+    
+    
+    func setUpNotification(notification: UILocalNotification) -> UILocalNotification {
+        notification.alertBody = "It's time to look 20m far from you for 20s !!"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.timeZone = NSTimeZone.systemTimeZone()
+        notification.repeatInterval = NSCalendarUnit.Hour
+        notification.soundName = "alarm.caf"
+        return notification
+    }
+    
+    
+    func publishLocalNotification(notification: UILocalNotification) -> UILocalNotification {
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        return notification
+    }
+    
+    
+    func updateElapsedTimeLabel(timer: NSTimer) {
+        
+        if stopwatch.elapsedTime >= 20.0 {
+            stopwatch.stop()
+        }
+        
+        if stopwatch.isRunning {
+            elapsedTimeLabel.text = stopwatch.elapsedTimeAsString
+        } else {
+            timer.invalidate()
         }
     }
     
@@ -49,57 +113,6 @@ class ViewController: UIViewController {
         } else {
             elapsedTimeLabel.text = "00.0"
             stopwatch.stop()
-        }
-    }
-    
-    func timerAction() {
-        
-        UIApplication.sharedApplication().cancelAllLocalNotifications()
-        let settings = UIApplication.sharedApplication().currentUserNotificationSettings()
-        
-        if settings!.types == .None {
-            let ac = UIAlertController(title: "Can't schedule", message: "Either we don't have permission to schedule notifications, or we haven't asked yet.", preferredStyle: .Alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-            presentViewController(ac, animated: true, completion: nil)
-            return
-        }
-        
-        let notification = UILocalNotification()
-        notification.alertBody = "It's time to look 20m far from you for 20s !!"
-        notification.soundName = UILocalNotificationDefaultSoundName
-        notification.fireDate = NSDate.init(timeIntervalSinceNow: Double(1))
-        notification.timeZone = NSTimeZone.systemTimeZone()
-        notification.repeatInterval = NSCalendarUnit.Hour
-        notification.soundName = "alarm.caf"
-        
-        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
-        UIApplication.sharedApplication().scheduleLocalNotification(notification)
-    
-        for i in 1..<4 {
-            let notificationR = UILocalNotification()
-            notificationR.alertBody = "It's time to look 20m far from you for 20s !!"
-            notificationR.soundName = UILocalNotificationDefaultSoundName
-            notificationR.fireDate = NSDate.init(timeIntervalSinceNow: Double(i * 20 * 60))
-            notificationR.timeZone = NSTimeZone.systemTimeZone()
-            notificationR.repeatInterval = NSCalendarUnit.Hour
-            notificationR.soundName = "alarm.caf"
-            
-            UIApplication.sharedApplication().applicationIconBadgeNumber = 0
-            UIApplication.sharedApplication().scheduleLocalNotification(notificationR)
-        }
-    }
-    
-    func updateElapsedTimeLabel(timer: NSTimer) {
-        
-        if stopwatch.elapsedTime >= 20.0
-        {
-            print("time up")
-            stopwatch.stop()
-        }
-        if stopwatch.isRunning {
-            elapsedTimeLabel.text = stopwatch.elapsedTimeAsString
-        } else {
-            timer.invalidate()
         }
     }
 }
